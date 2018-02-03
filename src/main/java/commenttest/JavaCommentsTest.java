ac51cfd2df71866f550263b2f9b5f2862553802f
dmytro-ndp <che@eclipse.org>
2018-02-03 22:40:43 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1517596725104
+//first_change1517694643545
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
