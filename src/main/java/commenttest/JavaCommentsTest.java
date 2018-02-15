47ac48ca28b414709b3bc279d35174337702f8f5
dmytro-ndp <user1518706594@1.com>
2018-02-15 16:08:24 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1518548493616
+//first_change1518706646933
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
